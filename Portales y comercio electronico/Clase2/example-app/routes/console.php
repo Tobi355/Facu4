c6d5cdf06161f98f5e3be2c1a61f0c522c7c68da
Tobias Blanco <tobiasurielblanco@gmail.com>
2026-04-06 11:47:56 -0300

    segunda clase y instalacion de laravel

---
+++ Portales y comercio electronico/Clase2/example-app/routes/console.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
+
+Artisan::command('hola', function(){
+    $this->comment('Hola Mundo');
+})-> purpose('dice hola mundo');
