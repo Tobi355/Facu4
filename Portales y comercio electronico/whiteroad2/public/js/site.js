c57349687b10390bf568600c4cdc7a20c14a32c4
Tobias Blanco <tobiasurielblanco@gmail.com>
2026-06-12 17:39:04 -0300

    2do parcial final agregado

--- Portales y comercio electronico/whiteroad2/public/js/site.js
+++
@@ -1,8 +0,0 @@
-// site.js
-// Stub file para evitar errores cuando el layout carga assets comunes.
-
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-    // Este archivo puede contener lógica común del sitio si se necesita.
-  });
-})();
